0b6e8f7f559ff6a76f5263c0656d4887dd067f91
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 18:18:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
 def my_each # put argument(s) here
-  # code here
+  # code heredef my_each(collections)
+  i = 0
+  while i < collections.length
+  yield (collections[i])
+  i += 1
+end
+collections
+end
 end
